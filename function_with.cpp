06a45c53f3be22cbbfe597d4338c7128ed8ecfb8
aditi saudagar <aditisaudagar17@gmail.com>
2024-11-29 13:18:57 +0530

    function

---
+++ function_with.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int roi()
+{
+return 3;
+}
+int balance (int pwd)
+{
+if(pwd==2003)
+{
+return 5000;
+}
+else
+{
+return -1;
+}
+}
+int main ()
+{
+    cout<<balance(2003);
+}
